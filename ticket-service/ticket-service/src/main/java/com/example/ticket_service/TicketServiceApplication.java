a2f4487d7a738fd6a9759c937e2f0046e5931d7d
SethminiDissanayake <sethminidissa@gmail.com>
2024-06-22 19:56:15 +0530

    appplication.yml file created

--- ticket-service/ticket-service/src/main/java/com/example/ticket_service/TicketServiceApplication.java
+++ ticket-service/ticket-service/src/main/java/com/example/ticket_service/TicketServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TicketServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TicketServiceApplication.class, args);
 	}
 
