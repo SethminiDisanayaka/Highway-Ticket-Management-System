da945e9a87e4dca6419365df31568d0069362f6a
SethminiDissanayake <sethminidissa@gmail.com>
2024-07-02 16:55:39 +0530

    Update Controller

--- ticket-service/ticket-service/src/main/java/com/example/ticket_service/contrroller/TicketController.java
+++ ticket-service/ticket-service/src/main/java/com/example/ticket_service/contrroller/TicketController.java
@@ -1,24 +1,56 @@
 package com.example.ticket_service.contrroller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import com.example.ticket_service.dto.TicketDTO;
+import com.example.ticket_service.services.TicketServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping("api/v1/tickets")
+@RequestMapping("/api/v1/ticketService")
 public class TicketController {
 
-    private final RestTemplate restTemplate;
+    @Autowired
+    TicketServices ticketServices;
 
-    public TicketController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    @PostMapping
+    public void checkHealth() {
+        System.out.println("Ticket Worked...");
     }
 
-    @GetMapping("/getTicket")
-    public String get() {
-        return "Hello from Customer Service";
+    @PostMapping
+    @RequestMapping("/createTicket")
+    public String createTicket(@RequestBody TicketDTO ticketDTO) {
+        String ticket = ticketServices.ticketCreate(ticketDTO);
+        return ticket;
     }
 
+    @PutMapping
+    @RequestMapping("/updateTicket/{updateTicketId}")
+    public String updateTicket(@PathVariable("updateTicketId") String updateTicketId, @RequestBody TicketDTO ticketDTO) {
+        String ticket = ticketServices.updateTicket(updateTicketId, ticketDTO);
+        return ticket;
+    }
 
+    @GetMapping
+    @RequestMapping("/getTicketDetails/{ticketId}")
+    public String getTicketDetails(@PathVariable("ticketId") String ticketId) {
+        String ticketDetails = ticketServices.getTicketDetails(ticketId);
+        return ticketDetails;
+    }
+
+    @GetMapping
+    @RequestMapping("/ticketHaveCheck/{ticketId}")
+    public boolean alreadyTicketHaveCheck(@PathVariable("ticketId") String ticketId) {
+        boolean b = ticketServices.alreadyTicketHaveCheck(ticketId);
+        return b;
+    }
+
+    @PostMapping
+    @RequestMapping("/statusUpdate/{ticketId}")
+    public String ticketStatusUpdate(@PathVariable("ticketId") String ticketId) {
+        ticketServices.ticketStatusUpdate(ticketId);
+        return "done";
+    }
 }
+
