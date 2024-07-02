abb8825193bde66d55fd8f0dee98d9c966c84e3b
SethminiDissanayake <sethminidissa@gmail.com>
2024-07-02 21:43:43 +0530

    updated

--- payment-service/payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
+++ payment-service/payment-service/src/main/java/com/example/payment_service/controller/PaymentController.java
@@ -1,23 +1,28 @@
 package com.example.payment_service.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.payment_service.dto.PaymentDTO;
+import com.example.payment_service.services.PaymentServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("api/v1/payments")
 public class PaymentController {
 
-    private final RestTemplate restTemplate;
+    @Autowired
+    PaymentServices paymentServices;
 
-    public PaymentController(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
+    @PostMapping
+    public void checkHealth(){
+        System.out.println("Payment worked...");
     }
 
-    @GetMapping("/getPayment")
-    public String get() {
-        return "Hello from Payment Service";
+    @PostMapping
+    @RequestMapping("/confirmPayment")
+    public String confirmPayment(@RequestBody PaymentDTO paymentDTO){
+        String confirmPaymentMassage = paymentServices.confirmPayment(paymentDTO);
+        return confirmPaymentMassage;
     }
 
 }
