2db20fdc0d67f61d6408d21701db6c1f106a0a50
SethminiDissanayake <sethminidissa@gmail.com>
2024-07-02 14:04:35 +0530

    entitypackage created

---
+++ user-service/user-service/src/main/java/com/example/user_service/entity/UserEntity.java
@@ -0,0 +1,21 @@
+package com.example.user_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
+public class UserEntity {
+    @Id
+    private String userId;
+    private String userMail;
+    private String userName;
+    private String password;
+}
